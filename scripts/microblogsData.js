de2829124049d9d376df5095f58b7bfda7420201
Pinkmin <jamespendon388@gmail.com>
2025-11-02 12:55:21 +0800

    [🔧]: Certain microblog posts are removed

--- scripts/microblogsData.js
+++ scripts/microblogsData.js
@@ -1,17 +1,5 @@
 export const MICROBLOGS = [
   {
-    date: '10/23/2025 8:04 PM',
-    content: `Because of my incompetency and lack of self-love, I couldn't even love the one I fell in love for.
-              I feel like all those I've been saying were just all an act and it was never real.
-              It was all my fault :/`,
-  },
-  {
-    date: '10/20/2025 3:46 PM',
-    content: `I like how I'm just stuck using neovim for years`,
-    tags: ['neovim', 'vim', 'texteditor'],
-  },
-
-  {
     date: '10/17/2025 9:34 PM',
     content: `I seriously need a Meisho Doto plushie 😞🙏
         <br />
